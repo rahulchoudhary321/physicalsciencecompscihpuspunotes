ef52f4398ef672b46d0a20dfa94149b8e27ed864
rahulchoudhary321 <131349881+rahulchoudhary321@users.noreply.github.com>
2024-10-27 19:48:49 +0530

    live

---
+++ index.js
@@ -0,0 +1,24 @@
+let icon = document.querySelector(".icon");
+let li1 = document.querySelector(".test1");
+let li2= document.querySelector(".test2");
+let li3= document.querySelector(".test3");
+let li4 = document.querySelector(".test4");
+let li5 = document.querySelector(".test5");
+let li6 = document.querySelector(".test6");
+let li7 = document.querySelector(".test7");
+
+
+icon.addEventListener("click", () => {
+    li1.classList.toggle("show");
+    li2.classList.toggle("show");
+    li3.classList.toggle("show");
+    li4.classList.toggle("show");
+    li5.classList.toggle("show");
+    li6.classList.toggle("show");
+    li7.classList.toggle("show");
+
+    
+ 
+})
+
+
